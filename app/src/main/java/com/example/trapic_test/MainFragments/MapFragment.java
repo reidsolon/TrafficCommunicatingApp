db704af9b3a987bb60b2a74acd8734f3cb9357fe
reidsolon <rayanthonysolon@gmail.com>
2019-03-17 22:14:48 +0800

    march 16 2:20pm

--- app/src/main/java/com/example/trapic_test/MainFragments/MapFragment.java
+++ app/src/main/java/com/example/trapic_test/MainFragments/MapFragment.java
@@ -9,20 +9,31 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.trapic_test.R;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.net.ConnectivityListener;
+import com.mapbox.mapboxsdk.net.ConnectivityReceiver;
 
-import java.util.Map;
+import java.util.List;
 
-public class MapFragment extends Fragment{
+public class MapFragment extends Fragment implements ConnectivityListener, PermissionsListener {
 
-    View view;
-    private MapView mapView;
+    MapboxMap mMap;
+    MapView mapView;
 
-    private GoogleMap mMap;
+    private PermissionsManager permissionsManager;
+    private LocationEngineProvider locationEngine;
+    private PermissionsListener permissionsListener;
     public MapFragment(){
 
     }
@@ -30,21 +41,121 @@ public class MapFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-
-        view = inflater.inflate(R.layout.map_layout, container, false);
-
-//        mapView = (MapView) view.findViewById(R.id.map);
-//
-//        MapsInitializer.initialize(getActivity().getApplicationContext());
-//
-//        mapView.getMapAsync(new OnMapReadyCallback() {
-//            @Override
-//            public void onMapReady(GoogleMap googleMap) {
-//                mMap = googleMap;
-//            }
-//        });
-        return view;
+        Mapbox.getInstance(getContext(), "pk.eyJ1IjoicmVpZHNvbG9uIiwiYSI6ImNqcnZpZThzMTAyN2Ezemx4eHMzM2RoZGwifQ.j65VGpYO6g84DnR1koippQ");
+        return inflater.inflate(R.layout.map_layout, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = view.findViewById(R.id.mapView);
+
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                enableLocationComponent();
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded(){
+
+
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+
+                    }
+                });
+
+            }
+        });
+
+
+    }
+
+    //
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationComponent() {
+
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(getContext())) {
+
+            // Get an instance of the component
+            LocationComponent locationComponent = mMap.getLocationComponent();
+
+            // Activate with options
+            locationComponent.activateLocationComponent(getContext(), mMap.getStyle());
+
+            // Enable to make component visible
+            locationComponent.setLocationComponentEnabled(true);
+
+            // Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING_GPS);
+
+            // Set the component's render mode
+            locationComponent.setRenderMode(RenderMode.NORMAL);
+
+
+        } else {
+
+            permissionsManager = new PermissionsManager((PermissionsListener) this);
+
+            permissionsManager.requestLocationPermissions(getActivity());
+
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onNetworkStateChanged(boolean connected) {
+
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if(granted){
+            enableLocationComponent();
+        }
+    }
 }
